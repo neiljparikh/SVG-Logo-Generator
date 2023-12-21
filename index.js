d43c8dfcb2eb796c806854fe08c050cf984bcd60
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2023-12-20 22:50:54 -0500

    Installed packages and built out prompting questions for user

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ const inquirer = require('inquirer');
 const SVG = require('svg.js');
 const fs = require('fs');
 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+        validate: input => input.length <= 3,
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal):',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal):',
+    }
+])
